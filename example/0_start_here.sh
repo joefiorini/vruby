b6f6e7f30b0328439eaaad58e02cdc351a067f7a
Joe Fiorini <joe@joefiorini.com>
2015-03-16 21:45:18 -0400

    Initial commit

---
+++ example/0_start_here.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# To get started execute this file with:
+#
+# ./0_start_here.sh
+#
+#
+
+../bin/vruby install --to=../rubies
+
+echo "Latest ruby installed to ../rubies. Next run \"../bin/vruby vruby\" and then \"source vruby/bin/activate\""
---
+++ example/1_rack_app.rb
@@ -0,0 +1,7 @@
+require 'rack'
+
+app = Proc.new do |env|
+    ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
+end
+
+Rack::Handler::WEBrick.run app
