79a0709fe7d820c71472e2c097ab8704b4334b03
Joe Fiorini <joe@joefiorini.com>
2015-03-18 09:33:21 -0400

    Support running example from root

--- example/0_start_here.sh
+++ example/0_start_here.sh
@@ -8,6 +8,16 @@
 
 install_to=../share
 
+# Make sure we're running from examples directory
+current_dir=$(basename $(pwd))
+
+if [[ "$current_dir" != "example" ]]; then
+  # Assumes we're at least in the vruby directory
+  cd example
+fi
+
+rm -rf ./vruby
+
 ../bin/vruby install --to $install_to
 
 echo "Latest ruby installed to $install_to"
