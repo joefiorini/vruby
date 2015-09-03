c56998d151c3ce47246a7a23fbb44c25a177e21e
Joe Fiorini <joe@joefiorini.com>
2015-09-02 23:47:55 -0400

    Implement deactivate command [fixes #19]

--- share/vruby/embed/activate.fish
+++ share/vruby/embed/activate.fish
@@ -1,3 +1,49 @@
+function deactivate -d "Exit vruby and return to normal shell environment"
+
+  # reset old environment variables
+  if set -q _OLD_VRUBY_PATH
+    set -gx PATH $_OLD_VRUBY_PATH
+    set -e _OLD_VRUBY_PATH
+  end
+
+  if set -q _OLD_VRUBY_RUBY_ROOT
+    set -gx RUBY_ROOT $_OLD_VRUBY_RUBY_ROOT
+    set -e _OLD_VRUBY_RUBY_ROOT
+  else
+    set -e RUBY_ROOT
+  end
+
+  if set -q _OLD_VRUBY_GEM_HOME
+    set -gx GEM_HOME $_OLD_VRUBY_GEM_HOME
+    set -e _OLD_VRUBY_GEM_HOME
+  else
+    set -e GEM_HOME
+  end
+
+  if set -q _OLD_VRUBY_GEM_PATH
+    set -gx GEM_PATH $_OLD_VRUBY_GEM_PATH
+    set -e _OLD_VRUBY_GEM_PATH
+  else
+    set -e GEM_PATH
+  end
+
+  set -e VRUBY
+
+  functions -e deactivate
+end
+
+if test -n $RUBY_ROOT
+  set -x _OLD_VRUBY_RUBY_ROOT $RUBY_ROOT
+end
+
+if test -n $GEM_HOME
+  set -x _OLD_VRUBY_GEM_HOME $GEM_HOME
+end
+
+if test -n $GEM_PATH
+  set -x _OLD_VRUBY_GEM_PATH $GEM_PATH
+end
+
 set RUBY_VERSION __VERSION__
 set RUBY_ENGINE ruby
 set RUBY_ROOT "__HOST_DIR__/__TARGET_DIR__"
@@ -5,6 +51,10 @@ set RUBY_ROOT "__HOST_DIR__/__TARGET_DIR__"
 set -x GEM_HOME "__HOST_DIR__/.gem/$RUBY_ENGINE/$RUBY_VERSION"
 set -x GEM_PATH "$GEM_HOME"
 
-set ruby_api_version (ruby -e "puts RbConfig::CONFIG['ruby_version']")
+set ruby_cmd $RUBY_ROOT/bin/ruby
+set ruby_api_version (eval $ruby_cmd -e "puts RbConfig::CONFIG['ruby_version']")
 
+set -x _OLD_VRUBY_PATH $PATH
 set -x PATH $GEM_HOME/bin "__FROM__/__VERSION__/lib/ruby/gems/$ruby_api_version" "$RUBY_ROOT/bin" $PATH
+
+set -x VRUBY "__HOST_DIR__/__TARGET_DIR__"
--- share/vruby/embed/activate.sh
+++ share/vruby/embed/activate.sh
@@ -1,11 +1,71 @@
+deactivate() {
+
+  if [ -n "${_OLD_VRUBY_PATH}" ]; then
+    echo "RESETTING PATH"
+    export PATH="$_OLD_VRUBY_PATH"
+    unset _OLD_VRUBY_PATH
+  fi
+
+  if [ -n "${_OLD_VRUBY_RUBY_ROOT}" ]; then
+    echo "RESETTING RUBY_ROOT"
+    export RUBY_ROOT="$_OLD_VRUBY_RUBY_ROOT"
+    unset _OLD_VRUBY_RUBY_ROOT
+  else
+    unset RUBY_ROOT
+  fi
+
+  if [ -n "${_OLD_VRUBY_GEM_HOME}" ]; then
+    echo "RESETTING GEM_HOME"
+    export GEM_HOME="$_OLD_VRUBY_GEM_HOME"
+    unset _OLD_VRUBY_GEM_HOME
+  else
+    unset GEM_HOME
+  fi
+
+  if [ -n "${_OLD_VRUBY_GEM_PATH}" ]; then
+    echo "RESETTING GEM_PATH"
+    export GEM_PATH="$_OLD_VRUBY_GEM_PATH"
+    unset _OLD_VRUBY_GEM_PATH
+  else
+    unset GEM_PATH
+  fi
+
+  unset VRUBY
+
+  unset -f deactivate
+}
+
 RUBY_VERSION=__VERSION__
 RUBY_ENGINE=ruby
+
+if [ -n "${RUBY_ROOT}" ]; then
+  _OLD_VRUBY_RUBY_ROOT="$RUBY_ROOT"
+  export _OLD_VRUBY_RUBY_ROOT
+fi
+
 RUBY_ROOT=__HOST_DIR__/__TARGET_DIR__
+
+export RUBY_ROOT=__HOST_DIR__/__TARGET_DIR__
+
+if [ -n "${GEM_HOME}" ]; then
+  _OLD_VRUBY_GEM_HOME="$GEM_HOME"
+  export _OLD_VRUBY_GEM_HOME
+fi
+
+export GEM_HOME="__HOST_DIR__/.gem/$RUBY_ENGINE/$RUBY_VERSION"
+
+if [ -n "${GEM_PATH}" ]; then
+  _OLD_VRUBY_GEM_PATH="$GEM_PATH"
+  export _OLD_VRUBY_GEM_PATH
+fi
+
 
 export GEM_HOME="__HOST_DIR__/.gem/$RUBY_ENGINE/$RUBY_VERSION"
 export GEM_PATH=$GEM_HOME
 
 ruby_api_version=$(ruby -e "puts RbConfig::CONFIG['ruby_version']")
 
+export _OLD_VRUBY_PATH="$PATH"
 export PATH=$GEM_HOME/bin:__FROM__/__VERSION__/lib/ruby/gems/$ruby_api_version/bin:$RUBY_ROOT/bin:$PATH
 
+export VRUBY="__HOST_DIR__/__TARGET_DIR__"
